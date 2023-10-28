a25cc45945630dbdcb5ad36de1ab08ff2342739f
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-10-28 14:49:43 +0200

    code js added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 
 let currentDPR = window.devicePixelRatio;
 
+const $inputs = document.querySelectorAll(`input`);
+const $buttons = document.querySelectorAll(`.number__pad button`);
+
+const buttonClickHandle = e => {
+    const emptyInput = Array.from($inputs).find((input) => input.value === ``);
+
+    if (emptyInput) {
+        emptyInput.value = e.target.textContent;
+    }
+
+    checkInputs();
+}
+
+const inputsHandle = e => {
+    checkInputs();
+}
+
+const checkInputs = () => {
+    const allInputsValid = Array.from($inputs).every((input) => input.checkValidity());
+
+    if (allInputsValid) {
+        window.location.href = `https://www.google.com`;
+    }
+}
+
 const init = () => {
     setInterval(() => {
         const newDPR = window.devicePixelRatio;
@@ -9,6 +34,14 @@ const init = () => {
         }
         currentDPR = newDPR;
     }, 1000);
+
+    $buttons.forEach($button => {
+        $button.addEventListener(`click`, buttonClickHandle)
+    });
+
+    $inputs.forEach($input => {
+        $input.addEventListener(`input`, inputsHandle)
+    });
 }
 
 init();
