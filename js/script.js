ba9903b5964a2db714829010be9cf69f785c6455
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-10-28 13:23:08 +0200

    favicon added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
- 
+
 let currentDPR = window.devicePixelRatio;
 
-const pages = ['index.html', 
-]
+const pages = ['', 'room1.html', 'room2.html', 'room3.html', 'room4.html', 'code.html']
 
-const keyDownHandle = e=>{
+const keyDownHandle = e => {
+    const currentPath = window.location.pathname;
+    const currentPage = currentPath.replace('/00-escaperoom/', '');
+    const currentIndex = pages.indexOf(currentPage);
+
     if (e.key === 'ArrowLeft') {
-        window.location.href = 'challenge1.html';
-    } else if (e.key === 'ArrowRight') {
-        window.location.href = 'challenge3.html';
+        window.location.href = pages[currentIndex - 1];
+    } else if (e.key === 'ArrowRight' && currentIndex != 0 && currentIndex != pages.length - 1) {
+        window.location.href = pages[currentIndex + 1];
     }
 }
 
-const init=()=>{
+const init = () => {
     setInterval(() => {
         const newDPR = window.devicePixelRatio;
         if (newDPR !== currentDPR) {
