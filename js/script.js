46f43de62c4e560787a398fe4277ae654c4dba3c
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-10-28 13:33:38 +0200

    js updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
 
 let currentDPR = window.devicePixelRatio;
 
-const pages = ['', 'room1.html', 'room2.html', 'room3.html', 'room4.html', 'code.html']
-
-const keyDownHandle = e => {
-    const currentPath = window.location.pathname;
-    const currentPage = currentPath.replace('/00-escaperoom/', '');
-    const currentIndex = pages.indexOf(currentPage);
-
-    if (e.key === 'ArrowLeft') {
-        window.location.href = pages[currentIndex - 1];
-    } else if (e.key === 'ArrowRight' && currentIndex != 0 && currentIndex != pages.length - 1) {
-        window.location.href = pages[currentIndex + 1];
-    }
-}
-
 const init = () => {
     setInterval(() => {
         const newDPR = window.devicePixelRatio;
@@ -23,9 +9,6 @@ const init = () => {
         }
         currentDPR = newDPR;
     }, 1000);
-
-
-    document.addEventListener('keydown', keyDownHandle)
 }
 
 init();
