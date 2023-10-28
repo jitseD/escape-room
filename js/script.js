697f5e012424c2cc46da639f97077c917c94345a
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2023-10-28 15:54:42 +0200

    code finished

--- js/script.js
+++ js/script.js
@@ -6,16 +6,22 @@ const $buttons = document.querySelectorAll(`.number__pad button`);
 
 const buttonClickHandle = e => {
     const emptyInput = Array.from($inputs).find((input) => input.value === ``);
+    const invalidInput = Array.from($inputs).find((input) => !input.checkValidity());
 
     if (emptyInput) {
         emptyInput.value = e.target.textContent;
+    } else if (invalidInput) {
+        invalidInput.value = e.target.textContent;
     }
 
     checkInputs();
 }
 
 const inputsHandle = e => {
-    checkInputs();
+    const noEmptyInputs = Array.from($inputs).every((input) => input.value !== ``);
+    if (noEmptyInputs) {
+        checkInputs();
+    }
 }
 
 const checkInputs = () => {
@@ -35,13 +41,15 @@ const init = () => {
         currentDPR = newDPR;
     }, 1000);
 
-    $buttons.forEach($button => {
-        $button.addEventListener(`click`, buttonClickHandle)
-    });
+    if (document.querySelector(`.code`)) {
+        $buttons.forEach($button => {
+            $button.addEventListener(`click`, buttonClickHandle)
+        });
 
-    $inputs.forEach($input => {
-        $input.addEventListener(`input`, inputsHandle)
-    });
+        $inputs.forEach($input => {
+            $input.addEventListener(`input`, inputsHandle)
+        });
+    }
 }
 
 init();
